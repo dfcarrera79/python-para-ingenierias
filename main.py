9e5e7a3b55cc486f3faa22c06331c6346d741bf8
dfcarrera79 <dfcarrera@outlook.com>
2023-03-11 11:16:27 -0500

    Nuevos cambios

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 # Datos (variables)
 
+# Definicion de constantes
+CADENA = 'Diego'
+
+# Definicion de variables
 anios = 34  # Int
-caracter = '1' 
-cadena = 'Diego'
+caracter = 'c' 
 booleano = True
-
 and_ = True
+caracter = ''
+num_complejo = 0.7071 + 0.7071j;
+num_complejo2 = 0.7071 - 0.7071j;
+numero = 11;
 
-# Funciones
-def mi_funcion(nombre, edad):
-	print(f'Hola {nombre}, tu edad es {edad}')
-	print(f'Hola {nombre}')
-    
-  
-# Llamadas a funciones
-mi_funcion('Diego', 34)
+
+# print('El producto de los numeros complejos: ', num_complejo*num_complejo2)
+
+if( numero % 2 == 0 ):
+  print(f'El numero {numero} es par')
+else:
+  print(f'El numero {numero} es impar')  
+
+# # Funciones
+# def mi_funcion(nombre, edad):
+# 	print(f'Hola {nombre}, tu edad es {edad}')
+# 	print(f'Hola {nombre}')
+ 
+# if( caracter and anios > 18 ):
+#   print('la condicion es verdadera ')
+# else:
+#   print('la condicion no es verdadera')  
+   
+# # Llamadas a funciones
+# mi_funcion('Diego', 34)
 
    
 
