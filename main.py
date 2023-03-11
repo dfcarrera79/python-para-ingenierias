43237838870ad983f5cf45085b85b12ba97f8412
dfcarrera79 <dfcarrera@outlook.com>
2023-03-11 12:01:34 -0500

    Nuevo codigo

--- main.py
+++ main.py
@@ -9,17 +9,34 @@ caracter = 'c'
 booleano = True
 and_ = True
 caracter = ''
-num_complejo = 0.7071 + 0.7071j;
-num_complejo2 = 0.7071 - 0.7071j;
-numero = 11;
+num_complejo = 0.7071 + 0.7071j
+num_complejo2 = 0.7071 - 0.7071j
+x = 11
+
+anios = str(18)
+
+print(len(anios))
+print(type(anios))
+# x **= 10
+
+# if ( (x % 2 == 0) ^ (anios > 18) ):
+# 	print('la condicion es verdadera ')
+# else:
+# 	print('la condicion es falsa')  	
 
 
-# print('El producto de los numeros complejos: ', num_complejo*num_complejo2)
+# print(4 >> 1);
 
-if( numero % 2 == 0 ):
-  print(f'El numero {numero} es par')
-else:
-  print(f'El numero {numero} es impar')  
+
+
+
+
+
+
+
+# y = x**2 + 2*x + 10
+
+# print(f"la funcion de x (y) es igual a: ", y);
 
 # # Funciones
 # def mi_funcion(nombre, edad):
@@ -33,6 +50,44 @@ else:
    
 # # Llamadas a funciones
 # mi_funcion('Diego', 34)
+
+
+# print('El producto de los numeros complejos: ', num_complejo*num_complejo2)
+
+# if( numero % 2 == 0 ):
+#   print(f'El numero {numero} es par')
+# else:
+#   print(f'El numero {numero} es impar')  
+
+
+
+# ### Decimal a binario
+# # número decimal 
+# numero_decimal = 168
+# print(f"El número decimal {numero_decimal} en binario es: {bin(numero_decimal)}")
+
+# ### Binario a decimal 
+# # número binario
+# numero_binario = "11000"
+# print(f"El número binario {numero_binario} en decimal es: {int(numero_binario, 2)}")
+
+# ### Decimal a hexadecimal
+# numero_decimal = 40
+# print(f"El número decimal {numero_decimal} en hexadecimal es: {hex(numero_decimal)}")
+
+# ### Binario a hexadecimal
+# binario = "11000"
+
+# print(f"El número binario {binario} en hexadecimal es: {hex(int(binario, 2))}")
+
+# ### Hexadecimal a decimal
+# # número hexadecimal
+# numero_hexadecimal = "3e7c"
+# print(f"El número hexadecimal {numero_hexadecimal} en decimal es: {int(numero_hexadecimal, 16)}")
+
+# ### Hexadecimal a binario
+# hexadecimal = "2f1"
+# print(f"El número hexadecimal {hexadecimal} en binario es: {bin(int(hexadecimal, 16))}")
 
    
 
