3fab8e6991bd0f1538281d12452985255ff3e6b5
dfcarrera79 <dfcarrera@outlook.com>
2023-03-11 10:26:41 -0500

    Comandos utilizados en tema 2 del curso de python para ingenierias

---
+++ main.py
@@ -0,0 +1,26 @@
+# Datos (variables)
+
+anios = 34  # Int
+caracter = '1' 
+cadena = 'Diego'
+booleano = True
+
+and_ = True
+
+# Funciones
+def mi_funcion(nombre, edad):
+	print(f'Hola {nombre}, tu edad es {edad}')
+	print(f'Hola {nombre}')
+    
+  
+# Llamadas a funciones
+mi_funcion('Diego', 34)
+
+   
+
+
+
+ 
+
+
+
