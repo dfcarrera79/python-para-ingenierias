f8a38030a8588651e413e4f194ab90c7637e9ba7
dfcarrera79 <dfcarrera@outlook.com>
2023-03-25 10:37:27 -0500

    Funciones con python

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+def calculadora(num1, num2, operacion):  
+  """
+  Funcion que realiza operaciones de suma, resta, multiplicacion y division a dos numeros de entrada
+  Argumentos:
+  num1, num2 -- son los numeros con los cuales vamos a realizar las operaciones
+  operacion  -- define el tipo de operacion que va a ejecutar la funcion calculadora (suma, resta, multiplicacion, division) 
+  """
+  if (operacion == 'sumar'):
+    return num1 + num2
+  elif (operacion == 'restar'):
+    return num1 - num2
+  elif (operacion == 'multiplicar'):
+    return num1 * num2
+  elif (operacion == 'dividir'):
+    if (num2 == 0):
+      print('No se puede dividir para cero')
+    else:  
+      return num1 / num2
+  else:
+    print(f'La operacion {operacion} no esta disponible en la calculadora')
+    
+def potencia(base, exponente):
+  """
+  Función que calcula la potencia de dos números.
+  Argumentos:
+  base ‐‐ base de la operación.
+  exponente ‐‐ exponente de la operación.
+  """
+  return base ** exponente  
---
+++ main.py
@@ -0,0 +1,37 @@
+# from calculadora import potencia
+
+# ### Funciones
+# def mi_funcion():
+#   print('Buenos dias')
+
+# copiar una  funcion en una variable78
+
+# nuevafuncion = mi_funcion
+
+# llamada a funciones
+# # mi_funcion()
+# # nuevafuncion()
+    
+# print('Bienvenido a la funcion calculadora')    
+# numero_1 = int(input('Ingrese el primer numero: '))
+# numero_2 = int(input('Ingrese el segundo numero: '))   
+# operacion = input('Que operacion desea realizar (sumar, restar, multiplicar o dividir): ')
+
+# print(potencia(2, 8))   
+# resultado = calculadora(numero_1, numero_2, operacion)   
+
+# print(f'El resultado de {operacion} {numero_1} con {numero_2} es: ', resultado)    
+
+import math
+
+# print(math.fabs(-1234))
+# print(math.gcd(34, 82))
+
+
+# print(math.ceil(198.19))
+# print(6%2)
+
+# arreglo = [1, 2, 3, 4, 5, 6, 7, 8]
+# print(sum(arreglo))
+
+print(math.log10(0.0007))
